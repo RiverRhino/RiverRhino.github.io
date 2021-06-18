67f4dc21f9cc12f068faf1d2b26c79748c6b2f24
RiverRhino <84433466+RiverRhino@users.noreply.github.com>
2021-06-18 02:37:15 -0700

    Add files via upload

---
+++ Scripts/Onclicks.js
@@ -0,0 +1,14 @@
+function ah(){
+    location = "jue.html";   //Acuerdate de cambiarlo al de github :3
+}
+
+function menujuegos(){
+ 
+    location = "Menu de juegos.html"; //Cambiar en Github
+}
+
+function VideoSorpresa(){
+
+    location = "";
+
+}
