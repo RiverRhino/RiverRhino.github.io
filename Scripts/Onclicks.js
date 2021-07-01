61db9c19d9547ed8e1bcf2e9a27c0f790db34078
RiverRhino <84433466+RiverRhino@users.noreply.github.com>
2021-06-30 18:09:41 -0700

    Add files via upload

--- Scripts/Onclicks.js
+++ Scripts/Onclicks.js
@@ -1,14 +1,17 @@
 function ah(){
-    location = "https://riverrhino.github.io/Ahorcado.html";   //Acuerdate de cambiarlo al de github :3
+    location = "https://riverrhino.github.io/Ahorcado.html";
 }
 
 function menujuegos(){
  
-    location = "https://riverrhino.github.io/Menu%20de%20juegos.html"; //Cambiar en Github
+    location = "https://riverrhino.github.io/Menu%20de%20juegos.html";
 }
 
 function Sorpresa(){
 
     location = "https://riverrhino.github.io/Sorpresa.html"
+}
 
+function Galeria(){
+    location = "https://riverrhino.github.io/Galeria.html"
 }
--- Scripts/Sorpresa.js
+++ Scripts/Sorpresa.js
@@ -1,12 +1,22 @@
 function verificar(){
-
+    
+    var cambiar = document.getElementById('Respuesta');
     let Adiv = document.querySelector('#Acertijo').value;
-    let ver = ["Acepto"];
+    let img = document.querySelector('#imag');
+    let ver = "Acepto";
     let mostrar = document.getElementById('BotonSorpresa');
     mostrar.style.display = 'none';
 
     if(Adiv == ver){
         mostrar.style.display='block';
+        cambiar.innerHTML = "Correcto :3";
+        img.src="imagenes/dinosaurio corazon.gif";
+    }
+    
+    if(Adiv != ver){
+        cambiar.innerHTML = "No es correcto :(";
+        img.src="imagenes/rikkatriste.gif";
+        
     }
 
 }
