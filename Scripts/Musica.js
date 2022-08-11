63c75202d6dd67c885a7b133b52ef8d1039cd9ba
RiverRhino <84433466+RiverRhino@users.noreply.github.com>
2022-08-10 18:15:00 -0700

    Delete Scripts directory

--- Scripts/Musica.js
+++
@@ -1,7 +0,0 @@
-let Musica = document.getElementById('Anillo');
-let Audio1 = document.querySelector('audio');
-
-Musica.addEventListener('click', ()=>{
-    Audio1.setAttribute("src","audios/Boda.mp3");
-    Audio1.play();
-});
--- Scripts/check.js
+++
@@ -1,7 +0,0 @@
-function Solve()
-{
-    let letter = document.getElementById('Check').value;
-    let tru = document.getElementById('True');
-
-
-}
