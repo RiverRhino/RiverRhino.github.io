02859f97d85314574f9330e4f3b624de4d46a6db
RiverRhino <oscarquinones45@gmail.com>
2026-07-28 19:51:46 -0700

    ghfghf

--- js/calculadora-teto.js
+++ js/calculadora-teto.js
@@ -30,7 +30,7 @@ async function reproducirFrase(texto) {
   try {
     // ya no llamamos a Fish Audio directo (eso causaba el error de CORS),
     // le pedimos el audio a nuestro propio servidor, que es quien tiene la key
-    const response = await fetch("https://riverrhino-github-io.onrender.com", {
+    const response = await fetch("https://riverrhino-github-io.onrender.com/api/tts", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
